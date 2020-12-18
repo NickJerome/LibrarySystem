2ca891e024f5b1bc4043b9a9186b0e81e120d6a7
Jerome <2464489338@qq.com>
2020-12-18 13:55:52 +0800

    [Update] 写了点交互逻辑awa

--- src/com/jerome/view/login.java
+++ src/com/jerome/view/login.java
@@ -1,4 +1,42 @@
 package com.jerome.view;
 
+import com.jerome.service.*;
+
+import java.util.Scanner;
+
 public class login {
+    public static void main (String[] argv) {
+        Scanner sc = new Scanner();
+
+        init(sc);
+    }
+
+    public void init (Scanner sc) {
+        while (true) {
+            System.out.println("---------------图书馆管理系统--------------");
+            System.out.println("【1】登录系统");
+            System.out.println("【2】退出系统");
+            System.out.println("----------------------------------------");
+
+
+            if (sc.nextInt() == 1) {
+                loginInit();
+            } else {
+                return ;
+            }
+        }
+    }
+
+    public void loginInit (Scanner sc) {
+        System.out.println("请输入账号：");
+        String userName = sc.nextLine();
+        System.out.println("请输入密码：");
+        String passWord = sc.nextLine();
+
+        if (userService.login(userName,passWord)) {
+
+        } else {
+            loginInit();
+        }
+    }
 }
